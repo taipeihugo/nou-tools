e6a508eb2e8616616b81e1dd2d3f40d3187bd044
BinotaLIU <binota@binota.org>
2026-04-19 14:01:02 +0800

    fix: remove default value for users.roles

--- database/migrations/2026_04_19_052421_add_roles_to_users_table.php
+++ database/migrations/2026_04_19_052421_add_roles_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('roles')->default('[]')->after('password');
+            $table->json('roles')->after('password');
         });
     }
 
