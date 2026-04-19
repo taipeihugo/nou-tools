7684fa368f4cfed57af7845f1f644295e40d7c12
BinotaLIU <binota@binota.org>
2026-04-19 14:09:48 +0800

    fix: handle default value for mysql

--- database/migrations/2026_04_19_052421_add_roles_to_users_table.php
+++ database/migrations/2026_04_19_052421_add_roles_to_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,8 +12,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->json('roles')->after('password');
+        $isSqlite = Schema::getConnection()->getDriverName() === 'sqlite';
+
+        Schema::table('users', function (Blueprint $table) use ($isSqlite) {
+            $table->json('roles')
+                ->default($isSqlite ? '[]' : DB::raw('JSON_ARRAY()'))
+                ->after('password');
         });
     }
 
